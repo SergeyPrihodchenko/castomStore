19488f1dd772daacb969b3fe57d971a3f88a3d28
Valentina Saraeva <kantor-07@mail.ru>
2023-11-04 20:48:17 +0300

    update styles adminPage

--- resources/js/Pages/Admin/Main/ui/Header/HeaderAdminPage.tsx
+++ resources/js/Pages/Admin/Main/ui/Header/HeaderAdminPage.tsx
@@ -94,92 +94,93 @@ export default function HeaderAdminPage() {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" color="primary">
           <Toolbar>
-            <Button
-              id="demo-customized-button"
-              aria-controls={open ? 'demo-customized-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              variant="contained"
-              disableElevation
-              onClick={handleClick}
-              //endIcon={<KeyboardArrowDownIcon />}
-            >
-              <MenuIcon />
-            </Button>
-            <StyledMenu
-              id="demo-customized-menu"
-              MenuListProps={{
-                'aria-labelledby': 'demo-customized-button',
-              }}
-              sx={{ marginLeft: '20px', marginTop: '-15px' }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>
-                <Button
-                  sx={{
-                    color: 'black',
-                    borderColor: 'black',
-                  }}
-                  variant="outlined"
-                  href="#outlined-buttons"
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{ flexGrow: 1, fontSize: '16px' }}
+            <>
+              <Button
+                id="demo-customized-button"
+                aria-controls={open ? 'demo-customized-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                //endIcon={<KeyboardArrowDownIcon />}
+              >
+                <MenuIcon />
+              </Button>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                sx={{ marginLeft: '20px', marginTop: '-15px' }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Button
+                    sx={{
+                      color: 'black',
+                      borderColor: 'black',
+                    }}
+                    variant="outlined"
+                    href="#outlined-buttons"
                   >
-                    Категории
-                  </Typography>
-                </Button>
-              </MenuItem>
-              <MenuItem onClick={handleClose} disableRipple>
-                <Button
-                  sx={{
-                    color: 'black',
-                    borderColor: 'black',
-                  }}
-                  variant="outlined"
-                  href="#outlined-buttons"
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{ flexGrow: 1, fontSize: '16px' }}
+                    <Typography
+                      variant="h6"
+                      sx={{ flexGrow: 1, fontSize: '16px' }}
+                    >
+                      Категории
+                    </Typography>
+                  </Button>
+                </MenuItem>
+                <MenuItem onClick={handleClose} disableRipple>
+                  <Button
+                    sx={{
+                      color: 'black',
+                      borderColor: 'black',
+                    }}
+                    variant="outlined"
+                    href="#outlined-buttons"
                   >
-                    Акции
-                  </Typography>
-                </Button>
-              </MenuItem>
-              <MenuItem onClick={handleClose} disableRipple>
-                <Button
-                  sx={{
-                    color: 'black',
-                    borderColor: 'black',
-                  }}
-                  variant="outlined"
-                  href="#outlined-buttons"
-                >
-                  <Typography
-                    variant="h6"
-                    sx={{ flexGrow: 1, fontSize: '16px' }}
+                    <Typography
+                      variant="h6"
+                      sx={{ flexGrow: 1, fontSize: '16px' }}
+                    >
+                      Акции
+                    </Typography>
+                  </Button>
+                </MenuItem>
+                <MenuItem onClick={handleClose} disableRipple>
+                  <Button
+                    sx={{
+                      color: 'black',
+                      borderColor: 'black',
+                    }}
+                    variant="outlined"
+                    href="#outlined-buttons"
                   >
-                    Новинки
-                  </Typography>
-                </Button>
-              </MenuItem>
-            </StyledMenu>
-
+                    <Typography
+                      variant="h6"
+                      sx={{ flexGrow: 1, fontSize: '16px' }}
+                    >
+                      Новинки
+                    </Typography>
+                  </Button>
+                </MenuItem>
+              </StyledMenu>
+            </>
             <Typography
               variant="h6"
               component="div"
-              sx={{ flexGrow: 1, fontWeight: 700 }}
+              sx={{ flexGrow: 1, fontWeight: 700, mr: 9 }}
             >
-              Твой магазин
+              Shop.com
             </Typography>
-            <Button color="inherit">
+            <Button color="inherit" sx={{ mr: -3 }}>
               <SearchIcon />
             </Button>
-            <Button color="inherit">
+            <Button color="inherit" sx={{ mr: -3 }}>
               <ShoppingBasketIcon />
             </Button>
             <Button
@@ -188,6 +189,7 @@ export default function HeaderAdminPage() {
               aria-haspopup="true"
               aria-expanded={open ? 'true' : undefined}
               onClick={handleClickProfile}
+              sx={{ mr: -3 }}
             >
               <AccountCircleIcon />
             </Button>
@@ -199,7 +201,7 @@ export default function HeaderAdminPage() {
               MenuListProps={{
                 'aria-labelledby': 'basic-button',
               }}
-              sx={{ marginLeft: '-20px', position: 'fixed' }}
+              sx={{ marginLeft: '-110px' }}
             >
               <MenuItem onClick={handleCloseProfile}>Профиль</MenuItem>
               <MenuItem onClick={handleCloseProfile}>Личный кабинет</MenuItem>
