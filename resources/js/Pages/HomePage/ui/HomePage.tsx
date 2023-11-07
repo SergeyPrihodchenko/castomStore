89d227563e8d4668982767ffcc3e2e8c11ad03b9
Valentina Saraeva <kantor-07@mail.ru>
2023-11-07 21:16:31 +0300

    update homePage

--- resources/js/Pages/HomePage/ui/HomePage.tsx
+++ resources/js/Pages/HomePage/ui/HomePage.tsx
@@ -3,6 +3,12 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SliderMobile from '@/image/SliderMobile.png';
+import Vector from '@/image/Vector.png';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 const theme = createTheme({
   palette: {
@@ -18,64 +24,95 @@ export default function MainPage() {
     <ThemeProvider theme={theme}>
       <Box
         sx={{
-          minWidth: '390px',
-          height: '800px',
-          alignContent: 'center',
+          marginTop: '20px',
         }}
       >
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{
-            flexGrow: 1,
-            fontWeight: 900,
-            mr: 4,
-            padding: '20px 0 0 40px',
-          }}
-        >
-          СОЗДАЙТЕ САЙТ ВАШЕЙ МЕЧТЫ
-        </Typography>
+        <Container fixed>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                fontWeight: 900,
+                alignItems: 'center',
+                paddingLeft: '20px',
+              }}
+            >
+              СОЗДАЙТЕ САЙТ ВАШЕЙ МЕЧТЫ
+            </Typography>
 
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{
-            flexGrow: 1,
-            fontSize: '14px',
-            alignContent: 'left',
-            padding: '0 0 10px 40px',
-          }}
-        >
-          У нас есть сайт, который вы можете настроить по своему усмотрению и
-          начать или расширить свою деятельность.
-        </Typography>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                fontSize: '14px',
+                alignContent: 'center',
+                paddingLeft: '20px',
+              }}
+            >
+              У нас есть сайт, который вы можете настроить по своему усмотрению
+              и начать или расширить свою деятельность.
+            </Typography>
 
-        <Button
-          variant="contained"
-          size="small"
-          sx={{
-            minWidth: '300px',
-            height: '16px',
-            padding: '40px',
-            alignContent: 'center',
-            borderRadius: '62px',
-            fontWeight: 400,
-            fontSize: '12px',
-            marginLeft: '40px',
-          }}
-          href="/dashboard"
-          color="primary"
-        >
-          Настройки магазина
-        </Button>
+            <Button
+              variant="contained"
+              size="small"
+              sx={{
+                width: '300px',
+                height: '52px',
+                alignContent: 'center',
+                borderRadius: '62px',
+                fontWeight: 400,
+                fontSize: '12px',
+                margin: '20px 0 20px 40px',
+              }}
+              href="/dashboard"
+              color="primary"
+            >
+              Настройки магазина
+            </Button>
+          </Box>
+        </Container>
         <Box
           sx={{
-            marginTop: '20px',
-            width: '390px',
-            height: '448px',
-            background: 'url(' + SliderMobile + ') no-repeat',
+            background: '#F2F0F1',
+            //paddingLeft: '20px',
           }}
-        />
+        >
+          <Paper
+            style={{
+              position: 'relative',
+              margin: '20px 0 50px 0',
+              minWidth: '390px',
+              height: '448px',
+              background: 'url(' + SliderMobile + ') no-repeat #F2F0F1',
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={8}></Grid>
+              <Grid item xs={4}>
+                <img src={Vector} width={'76'} height={'76'} alt="vectorBig" />
+              </Grid>
+              <Grid item xs={4}>
+                <img
+                  src={Vector}
+                  width={'44'}
+                  height={'44'}
+                  alt="vectorSmall"
+                />
+              </Grid>
+              <Grid item xs={8}></Grid>
+            </Grid>
+          </Paper>
+        </Box>
       </Box>
     </ThemeProvider>
   );
