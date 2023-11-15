12a37b93e87c51a9a13a155cbdda7b321af40fe6
Sergo <S.prihodchenko@yandex.ru>
2023-11-15 12:38:35 +0300

    a

--- resources/js/Pages/HomePage/HomePage.tsx
+++ resources/js/Pages/HomePage/HomePage.tsx
@@ -3,14 +3,13 @@ import HomePage from './ui/HomePage';
 import Footer from '@/Widgets/Footer';
 import Box from '@mui/material/Box';
 import Categories from '@/Features/Admin/CatalogsPanel/ui/Categories';
-import CategoriesPage from '../AdminPage/Form/ui/CategoriesPage';
 import Catalogs from '@/Features/Admin/CatalogsPanel/ui/Catalogs';
 
 export default function Welcome() {
   return (
     <Box sx={{ minWidth: '390px' }}>
       <Header />
-      <Catalogs/>
+      <Catalogs/>  {/*подключал для тестирования*/}
       <Footer />
     </Box>
   );
