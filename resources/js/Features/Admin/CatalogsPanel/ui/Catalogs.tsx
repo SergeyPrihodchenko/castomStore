24fbc2feb69722a38e1eb3a7a458abf8e925a8df
Sergo <S.prihodchenko@yandex.ru>
2023-11-17 14:26:48 +0300

    a

--- resources/js/Features/Admin/CatalogsPanel/ui/Catalogs.tsx
+++ resources/js/Features/Admin/CatalogsPanel/ui/Catalogs.tsx
@@ -1,7 +1,6 @@
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
 import TableContainer from '@mui/material/TableContainer';
 import TableFooter from '@mui/material/TableFooter';
 import TablePagination from '@mui/material/TablePagination';
@@ -10,177 +9,107 @@ import Paper from '@mui/material/Paper';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
-import SortIcon from '@mui/icons-material/Sort';
-import Box from '@mui/material/Box';
-
 import { useState } from 'react';
 import TablePaginationActions from './TablePaginationAction';
-import {
-  useSetCatalogMutation,
-  useSetCategoryMutation,
-  useUpdateCatalogForCategoryMutation,
-  useUpdateCatalogMutation,
-  useUpdateCategoryMutation,
-  useDeleteCatalogMutation,
-  useDeleteCategoryMutation,
-  useGetCatalogsQuery,
-  useGetCategoriesQuery,
-} from '../model/reducers/query/rtkCatalogs';
+import { useDeleteCatalogMutation, useGetCatalogsQuery } from "../model/reducers/query/rtkCatalogs"  
 
-export default function Catalogs() {
-  const { data: catalogs, isSuccess } = useGetCatalogsQuery('');
+  
+  export default function Catalogs() {
 
-  const [deleteCatalog, {}] = useDeleteCatalogMutation();
+    const {data: catalogs, isSuccess} = useGetCatalogsQuery('')
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [deleteCatalog, {}] = useDeleteCatalogMutation()
+    
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+        
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - (isSuccess ? catalogs.length : 0)) : 0;
+  
+    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - (isSuccess ? catalogs.length : 0)) : 0;
-
-  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  return (
-    <TableContainer component={Paper}>
-      <Table
-
-        style={{ minWidth: 290 }}
-        aria-label="custom pagination table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell
-              style={{
-                fontFamily: 'Satoshi',
-                fontWeight: '700',
-                fontSize: '18px',
-                display: 'flex',
-              }}
-            >
-              <IconButton
-                edge="end"
-                aria-label="edit"
-                //onClick={}
-                sx={{ marginRight: '10px' }}
-              >
-                <SortIcon />
-              </IconButton>
-              <Box sx={{ marginTop: '7px' }}> Наименование</Box>
-            </TableCell>
-            <TableCell
-              align="left"
-              style={{
-                fontFamily: 'Satoshi',
-                fontWeight: '700',
-                fontSize: '18px',
-              }}
-            >
-              Изм
-            </TableCell>
-            <TableCell
-              align="left"
-              style={{
-                fontFamily: 'Satoshi',
-                fontWeight: '700',
-                fontSize: '18px',
-              }}
-            >
-              Уд
-            </TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {(rowsPerPage > 0
-            ? isSuccess
-              ? catalogs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : []
-            : isSuccess
-            ? catalogs
-            : []
-          ).map((catalog) => (
-            <TableRow key={catalog.id}>
-              <TableCell
-                component="th"
-                scope="row"
-
-                style={{
-                  fontFamily: 'Satoshi',
-                  fontWeight: '500',
-                  fontSize: '18px',
+    return (
+      <TableContainer component={Paper}>
+        <Table
+          sx={{ minWidth: 290 }}
+          aria-label="custom pagination table"
+        >
+          <TableBody>
+            {(rowsPerPage > 0
+              ? (isSuccess ? catalogs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : [])
+              : (isSuccess ? catalogs : [])
+            ).map((catalog) => (
+              <TableRow key={catalog.id}>
+                <TableCell
+                  component="th"
+                  scope="row"
+                >
+                  {catalog.title}
+                </TableCell>
+                <TableCell
+                  style={{ width: 10 }}
+                  align="right"
+                >
+                  <IconButton
+                    edge="end"
+                    aria-label="edit"
+                    href="#"
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell
+                  style={{ width: 10 }}
+                  align="right"
+                >
+                  <IconButton
+                    edge="end"
+                    aria-label="edit"
+                    href="#"
+                    onClick={() => {deleteCatalog(catalog.id)}}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={isSuccess ? catalogs.length : 0}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    'aria-label': 'rows per page',
+                  },
+                  native: true,
                 }}
-
-              >
-                {catalog.title}
-              </TableCell>
-              <TableCell
-                style={{ width: 10 }}
-                align="right"
-              >
-                <IconButton
-                  edge="end"
-                  aria-label="edit"
-                  href="#"
-                >
-                  <EditIcon />
-                </IconButton>
-              </TableCell>
-              <TableCell
-                style={{ width: 10 }}
-                align="right"
-              >
-                <IconButton
-                  edge="end"
-                  aria-label="edit"
-                  href="#"
-                  onClick={() => {
-                    deleteCatalog(catalog.id);
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </TableCell>
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
             </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-
-              rowsPerPageOptions={[5, 10, 25, { label: 'Все', value: -1 }]}
-
-              colSpan={3}
-              count={isSuccess ? catalogs.length : 0}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-  );
-}
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    );
+  }
+  
